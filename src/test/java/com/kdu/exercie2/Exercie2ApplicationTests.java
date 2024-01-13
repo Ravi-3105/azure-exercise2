a60ce3e798508d39192d334d9386468fa49dd2c2
Ravi <raviranjan.maurya@kickdrum.tech.com>
2024-01-13 09:21:15 +0530

    azure exercise 2 code

---
+++ src/test/java/com/kdu/exercie2/Exercie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdu.exercie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
